092fdff1fa803265229833394245581b9c1a4df3
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-07-22 21:44:19 -0300

    Create getAbsoluteURL

---
+++ src/pages/login.tsx
@@ -0,0 +1,40 @@
+import { withAuthUser, AuthAction } from "next-firebase-auth";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useState } from "react";
+
+const MyLoader = () => <div>Loading...</div>;
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="E-mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={() => handleLogin()}>Entrar</button>
+    </>
+  );
+};
+
+export default withAuthUser({
+  whenAuthed: AuthAction.REDIRECT_TO_APP,
+  whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+  whenUnauthedAfterInit: AuthAction.RENDER,
+  LoaderComponent: MyLoader,
+})(LoginPage);
---
+++ src/utils/getAbsoluteURL.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest } from "next"
+
+const getAbsoluteURL = (url: string, req: NextApiRequest | null = null) => {
+  let host: string | undefined;
+
+  if (req) {
+    host = req.headers.host
+  } else {
+    if (typeof window === 'undefined') {
+      throw new Error(
+        'The "req" parameter must be provided if on the server side.'
+      )
+    }
+    host = window.location.host
+  }
+  const isLocalhost = host?.indexOf('localhost') === 0
+  const protocol = isLocalhost ? 'http' : 'https'
+  return `${protocol}://${host}${url}`
+}
+
+export default getAbsoluteURL
