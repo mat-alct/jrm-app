bed2dd0a4b51192c873824fc9db518999c9313f7
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-07-22 21:03:26 -0300

    Add basic configurations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import initAuth from "../services/initAuth";
+
+initAuth();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
