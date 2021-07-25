8a3724dad8201bba205c3acb8c477e4003495b3a
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-07-25 17:23:15 -0300

    Add images to project

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import 'firebase/auth';
 
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex, Heading, Input } from '@chakra-ui/react';
 import firebase from 'firebase/app';
 import { AuthAction, withAuthUser } from 'next-firebase-auth';
 import { useState } from 'react';
@@ -16,28 +16,49 @@ const LoginPage = () => {
   };
 
   return (
-    <>
-      <input
-        type="text"
-        placeholder="E-mail"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Senha"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <button type="button" onClick={() => handleLogin()}>
-        Entrar
-      </button>
-    </>
+    <Flex as="main" align="stretch" h="100vh">
+      <Flex flex="1" background="black" />
+      <Flex as="section" w="100%" maxW="44em" align="center" justify="center">
+        <Flex>
+          <form
+            style={{
+              maxWidth: '22em',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              gap: '0.5em',
+            }}
+          >
+            <Heading size="lg" mb="1em">
+              Entre já na sua conta
+            </Heading>
+            <Input
+              type="text"
+              placeholder="E-mail"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              w="100%"
+            />
+            <Input
+              type="password"
+              placeholder="Senha"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              w="100%"
+            />
+            <Button type="submit" w="100%" onClick={() => handleLogin()}>
+              Entrar
+            </Button>
+          </form>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
 
 export default withAuthUser({
-  whenAuthed: AuthAction.REDIRECT_TO_APP,
+  whenAuthed: AuthAction.RENDER,
   whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
   whenUnauthedAfterInit: AuthAction.RENDER,
   LoaderComponent: MyLoader,
