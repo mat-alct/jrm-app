1d80006cee54aaad3250f9891a9c869b64c85eb4
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-07-26 06:38:56 -0300

    Add react-hook-form to login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,17 +3,26 @@ import 'firebase/auth';
 import { EmailIcon, LockIcon } from '@chakra-ui/icons';
 import {
   Button,
+  Container,
   Flex,
   FormControl,
+  FormErrorMessage,
   Heading,
   Input,
   InputGroup,
   InputLeftElement,
   Spinner,
 } from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import firebase from 'firebase/app';
 import { AuthAction, withAuthUser } from 'next-firebase-auth';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import * as Yup from 'yup';
+
+interface HandleLoginProps {
+  email: string;
+  password: string;
+}
 
 const MyLoader = () => (
   <div
@@ -28,14 +37,26 @@ const MyLoader = () => (
   </div>
 );
 const LoginPage = () => {
-  const handleLogin = async (email: string, password: string) => {
-    await firebase.auth().signInWithEmailAndPassword(email, password);
-  };
-
   const validationSchema = Yup.object().shape({
     email: Yup.string().required('Email obrigatório'),
-    password: Yup.string().required('Senha obrigatória'),
+    password: Yup.string()
+      .required('Senha obrigatória')
+      .min(8, 'Senha precisa de no mínimo 8 dígitos'),
   });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<HandleLoginProps>({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const handleLogin: SubmitHandler<HandleLoginProps> = async submitData => {
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(submitData.email, submitData.password);
+  };
 
   return (
     <Flex as="main" align="stretch" h="100vh">
@@ -55,28 +76,57 @@ const LoginPage = () => {
             <Heading size="lg" mb="1em">
               Entre já na sua conta
             </Heading>
+            <form
+              noValidate
+              onSubmit={handleSubmit(handleLogin)}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '0.5em',
+                width: '100%',
+                maxWidth: '21em',
+              }}
+            >
+              <FormControl isInvalid={!!errors.email}>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    <EmailIcon color="gray.300" size={20} />
+                  </InputLeftElement>
+                  <Input
+                    id="email"
+                    {...register('email')}
+                    type="email"
+                    placeholder="E-mail"
+                    defaultValue="mateus@jrmcompensados.com"
+                  />
+                </InputGroup>
+                <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={!!errors.password}>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    <LockIcon color="gray.300" size={20} />
+                  </InputLeftElement>
+                  <Input
+                    id="password"
+                    {...register('password')}
+                    type="password"
+                    placeholder="Senha"
+                    defaultValue="12345678"
+                  />
+                </InputGroup>
+                <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+              </FormControl>
 
-            <FormControl gap="1em">
-              <InputGroup>
-                <InputLeftElement pointerEvents="none">
-                  <EmailIcon color="gray.300" size={20} />
-                </InputLeftElement>
-                <Input type="email" placeholder="E-mail" />
-              </InputGroup>
-            </FormControl>
-
-            <FormControl>
-              <InputGroup>
-                <InputLeftElement pointerEvents="none">
-                  <LockIcon color="gray.300" size={20} />
-                </InputLeftElement>
-                <Input type="password" placeholder="Senha" />
-              </InputGroup>
-            </FormControl>
-
-            <Button variant="solid" type="submit" isFullWidth>
-              Entrar
-            </Button>
+              <Button
+                isLoading={isSubmitting}
+                variant="solid"
+                type="submit"
+                isFullWidth
+              >
+                Entrar
+              </Button>
+            </form>
           </Flex>
         </Flex>
       </Flex>
