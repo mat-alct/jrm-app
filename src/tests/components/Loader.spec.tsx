019fff6f44f58173590b2975251025db5019f989
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-08-10 14:06:41 -0300

    Add Loader tests

---
+++ src/tests/components/Loader.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import { Loader } from '../../components/Loader';
+
+describe('Component: Loader', () => {
+  it('render the component', () => {
+    render(<Loader />);
+  });
+});
