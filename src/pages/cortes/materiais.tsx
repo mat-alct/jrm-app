785a4851d443b4b23ab8c1f18015d30ae700f114
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-08-22 09:14:00 -0300

    Create new material modal form

--- src/pages/cortes/materiais.tsx
+++ src/pages/cortes/materiais.tsx
@@ -10,12 +10,14 @@ import {
   Thead,
   Tr,
   useDisclosure,
+  VStack,
 } from '@chakra-ui/react';
 import React from 'react';
 import { FaEdit, FaTrash } from 'react-icons/fa';
 
 import { Dashboard } from '../../components/Dashboard';
 import { Header } from '../../components/Dashboard/Content/Header';
+import { FormInput } from '../../components/Form/Input';
 import { FormModal } from '../../components/Modal/FormModal';
 
 const Materiais = () => {
@@ -30,7 +32,14 @@ const Materiais = () => {
             Novo Material
           </Button>
         </Header>
-        <FormModal isOpen={isOpen} onClose={onClose} title="Novo Material" />
+        <FormModal isOpen={isOpen} onClose={onClose} title="Novo Material">
+          <VStack as="form" spacing={4} mx="auto">
+            <FormInput maxWidth="none" name="Material" placeholder="Material" />
+            <FormInput maxWidth="none" name="Largura" placeholder="Largura" />
+            <FormInput maxWidth="none" name="Altura" placeholder="Altura" />
+            <FormInput maxWidth="none" name="Preço" placeholder="Preço" />
+          </VStack>
+        </FormModal>
         <Table variant="striped" colorScheme="orange">
           <TableCaption>Lista de Materiais</TableCaption>
           <Thead>
