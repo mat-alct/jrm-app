83633c448aadefdf8c0e729822e441afca7a7768
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-08-22 08:49:14 -0300

    Finish materials table

--- src/pages/cortes/materiais.tsx
+++ src/pages/cortes/materiais.tsx
@@ -1,5 +1,17 @@
-import { Button, Flex, Heading, HStack } from '@chakra-ui/react';
+import {
+  Button,
+  HStack,
+  IconButton,
+  Table,
+  TableCaption,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 import { Dashboard } from '../../components/Dashboard';
 import { Header } from '../../components/Dashboard/Content/Header';
@@ -12,6 +24,64 @@ const Materiais = () => {
           <Button colorScheme="gray">Atualizar</Button>
           <Button colorScheme="orange">Novo Material</Button>
         </Header>
+        <Table variant="striped" colorScheme="orange">
+          <TableCaption>Lista de Materiais</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Material</Th>
+              <Th isNumeric>Largura (mm)</Th>
+              <Th isNumeric>Altura (mm)</Th>
+              <Th isNumeric>Preço</Th>
+              <Th />
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>MDF BRANCO TX 2 FACES COMUM 15MM </Td>
+              <Td isNumeric>2750</Td>
+              <Td isNumeric>1850</Td>
+              <Td isNumeric>R$ 239</Td>
+              <Td>
+                <HStack spacing={4}>
+                  <IconButton
+                    colorScheme="orange"
+                    size="sm"
+                    aria-label="Editar"
+                    icon={<FaEdit />}
+                  />
+                  <IconButton
+                    colorScheme="orange"
+                    size="sm"
+                    aria-label="Remover"
+                    icon={<FaTrash />}
+                  />
+                </HStack>
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>MDF BRANCO TX 2 FACES COMUM 15MM </Td>
+              <Td isNumeric>2750</Td>
+              <Td isNumeric>1850</Td>
+              <Td isNumeric>R$ 239</Td>
+              <Td>
+                <HStack spacing={4}>
+                  <IconButton
+                    colorScheme="orange"
+                    size="sm"
+                    aria-label="Editar"
+                    icon={<FaEdit />}
+                  />
+                  <IconButton
+                    colorScheme="orange"
+                    size="sm"
+                    aria-label="Remover"
+                    icon={<FaTrash />}
+                  />
+                </HStack>
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
       </Dashboard>
     </>
   );
