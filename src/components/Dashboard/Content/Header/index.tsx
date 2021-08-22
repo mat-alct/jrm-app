79969337311f0c98b0f95ef01baf41af9b9b1150
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-08-22 08:33:41 -0300

    Add Header component inside Content and update it

---
+++ src/components/Dashboard/Content/Header/index.tsx
@@ -0,0 +1,15 @@
+import { Flex, Heading, HStack } from '@chakra-ui/react';
+
+interface HeaderProps {
+  pageTitle: string;
+}
+
+export const Header: React.FC<HeaderProps> = ({ pageTitle, children }) => {
+  return (
+    <Flex align="center" justify="space-between" mb={8}>
+      <Heading color="gray.700">{pageTitle}</Heading>
+
+      <HStack spacing={8}>{children}</HStack>
+    </Flex>
+  );
+};
