1e05cfa239b9adbf94ca38ef807f977f06f92cd5
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-08-22 10:33:12 -0300

    Add react query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import React from 'react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import initAuth from '../services/initAuth';
+import { queryClient } from '../services/queryClient';
 import { theme } from '../styles/theme';
 
 initAuth();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 export default MyApp;
