678f71680c40eafe2e4009b1a2728b802aeddd83
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-09-07 18:10:37 -0300

    fix errors in api folder

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,17 +1,17 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { setAuthCookies } from 'next-firebase-auth'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { setAuthCookies } from 'next-firebase-auth';
+
 import initAuth from '../../services/initAuth';
 
-initAuth()
+initAuth();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    await setAuthCookies(req, res)
+    await setAuthCookies(req, res);
   } catch (e) {
-    console.error(e)
-    return res.status(500).json({ error: 'Unexpected error.' })
+    return res.status(500).json({ error: 'Unexpected error.' });
   }
-  return res.status(200).json({ status: true })
-}
+  return res.status(200).json({ status: true });
+};
 
-export default handler
+export default handler;
--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,18 +1,17 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { unsetAuthCookies } from 'next-firebase-auth'
-import initAuth from '../../services/initAuth'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { unsetAuthCookies } from 'next-firebase-auth';
 
-initAuth()
+import initAuth from '../../services/initAuth';
+
+initAuth();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    await unsetAuthCookies(req, res)
+    await unsetAuthCookies(req, res);
   } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e)
-    return res.status(500).json({ error: 'Unexpected error.' })
+    return res.status(500).json({ error: 'Unexpected error.' });
   }
-  return res.status(200).json({ status: true })
-}
+  return res.status(200).json({ status: true });
+};
 
-export default handler
+export default handler;
