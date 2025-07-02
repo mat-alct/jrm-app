d3693924a892ed84ce928a2baee4dbb94955e9c8
mat-alcantara <mateuscastro.al@gmail.com>
2025-07-02 17:12:55 -0300

    update theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,36 +1,41 @@
-import { extendTheme } from '@chakra-ui/react';
+import { createSystem, defaultConfig } from '@chakra-ui/react';
 
-export const theme = extendTheme({
-  colors: {
-    gray: {
-      '900': '#181B23',
-      '800': '#1F2029',
-      '700': '#353646',
-      '600': '#4B4D63',
-      '500': '#616480',
-      '400': '#797D94',
-      '300': '#9699B0',
-      '200': '#B3B5C6',
-      '100': '#D1D2DC',
-      '50': '#EEEEF2',
-    },
-  },
-  fonts: {
-    heading: 'Roboto',
-    body: 'Roboto',
-  },
-  styles: {
-    global: {
-      body: {
-        color: 'gray.800',
+export const theme = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      colors: {
+        gray: {
+          900:{value: '#181B23'} ,
+          800: {value: '#1F2029'},
+          700: {value: '#353646'} ,
+          600: {value: '#4B4D63'} ,
+          500: {value: '#616480'} ,
+          400: {value: '#797D94'} ,
+          300: {value: '#9699B0'} ,
+          200: {value: '#B3B5C6'} ,
+          100: {value: '#D1D2DC'} ,
+          50: {value:'#EEEEF2'},
+        },
       },
-    },
-  },
-  components: {
-    Button: {
-      baseStyle: {
-        cursor: 'pointer',
+      fonts: {
+        heading: {value: 'Roboto'} ,
+        body: {value: 'Roboto'} ,
       },
-    },
-  },
+      styles: {
+        global: {
+          body: {
+            color: {value: 'gray.800'} ,
+          },
+        },
+      },
+      components: {
+        Button: {
+          baseStyle: {
+            cursor: 'pointer',
+          },
+        },
+      },
+    }
+  }
+
 });
